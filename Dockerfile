271c9145eab8a0c1e10dfd3da38925604b530efe
Pedro Correa <peter@Pedros-MBP.home>
2023-06-16 23:34:02 -0400

    minor edits

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,36 @@
-FROM ubuntu:20.04
+# base image
+FROM --platform=linux/amd64 ubuntu:20.04
 WORKDIR  /usr/src/app
-ARG DEBIAN_FRONTEND=noninteractive
-RUN apt update && apt install -y build-essential ruby cmake git
-EXPOSE 3000
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# update the base packages + add a non-sudo user
+RUN apt-get update -y && apt-get upgrade -y && useradd -m docker
+
+# install the packages and dependencies along with jq so we can parse JSON (add additional packages as necessary)
+RUN apt-get install -y --no-install-recommends sudo ruby cmake curl nodejs wget unzip vim git jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev python3-pip
+
+# cd into the user directory, download and unzip the github actions runner
+RUN mkdir actions-runner && cd actions-runner && curl -o actions-runner-linux-x64-2.305.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.305.0/actions-runner-linux-x64-2.305.0.tar.gz && tar xzf ./actions-runner-linux-x64-2.305.0.tar.gz
+
+# install some additional dependencies
+RUN chown -R docker ~docker && actions-runner/bin/installdependencies.sh
+
+# set the user to "docker" so all subsequent commands are run as the docker user
+USER docker
+
+# set the entrypoint to the start.sh script
 CMD ["/bin/bash"]
 
-# docker build -t devbox .
-# docker run -it devbox bash
-#  docker compose up -d
+# docker compose up -d
 # docker exec -it <ID> bash
+# cd /usr/src/app
+
+# apt update && apt install -y curl
+# RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/cu-ecen-aeld/assignment-1-petercorrea --token AHWXQGIVCTGZ2PHSSND2Y6DERRRQE
+# ./bin/installdependencies.sh
+# export GITHUB_ACTIONS_RUNNER_TLS_NO_VERIFY=1
+# RUNNER_ALLOW_RUNASROOT="1" ./run.sh
+
+# sudo service docker status
+# sudo service docker start
--- actions-runner/svc.sh
+++ actions-runner/svc.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-SVC_NAME="actions.runner.cu-ecen-aeld-assignment-1-petercorrea.Pedros-MBP"
+SVC_NAME="actions.runner.cu-ecen-aeld-assignment-1-petercorrea.5398ccb646be.service"
 SVC_NAME=${SVC_NAME// /_}
-SVC_DESCRIPTION="GitHub Actions Runner (cu-ecen-aeld-assignment-1-petercorrea.Pedros-MBP)"
-
-user_id=`id -u`
-
-# launchctl should not run as sudo for launch runners
-if [ $user_id -eq 0 ]; then
-    echo "Must not run with sudo"
-    exit 1
-fi
+SVC_DESCRIPTION="GitHub Actions Runner (cu-ecen-aeld-assignment-1-petercorrea.5398ccb646be)"
 
 SVC_CMD=$1
+arg_2=${2}
+
 RUNNER_ROOT=`pwd`
 
-LAUNCH_PATH="${HOME}/Library/LaunchAgents"
-PLIST_PATH="${LAUNCH_PATH}/${SVC_NAME}.plist"
+UNIT_PATH=/etc/systemd/system/${SVC_NAME}
 TEMPLATE_PATH=$GITHUB_ACTIONS_RUNNER_SERVICE_TEMPLATE
 IS_CUSTOM_TEMPLATE=0
 if [[ -z $TEMPLATE_PATH ]]; then
-    TEMPLATE_PATH=./bin/actions.runner.plist.template
+    TEMPLATE_PATH=./bin/actions.runner.service.template
 else
     IS_CUSTOM_TEMPLATE=1
 fi
-TEMP_PATH=./bin/actions.runner.plist.temp
+TEMP_PATH=./bin/actions.runner.service.temp
 CONFIG_PATH=.service
+
+user_id=`id -u`
+
+# systemctl must run as sudo
+# this script is a convenience wrapper around systemctl
+if [ $user_id -ne 0 ]; then
+    echo "Must run as sudo"
+    exit 1
+fi
 
 function failed()
 {
@@ -42,88 +44,112 @@ if [ ! -f "${TEMPLATE_PATH}" ]; then
     fi
 fi
 
+#check if we run as root
+if [[ $(id -u) != "0" ]]; then
+    echo "Failed: This script requires to run with sudo." >&2
+    exit 1
+fi
+
 function install()
 {
-    echo "Creating launch runner in ${PLIST_PATH}"
-
-    if [ ! -d  "${LAUNCH_PATH}" ]; then
-        mkdir ${LAUNCH_PATH}
-    fi
-
-    if [ -f "${PLIST_PATH}" ]; then
-        failed "error: exists ${PLIST_PATH}"
+    echo "Creating launch runner in ${UNIT_PATH}"
+    if [ -f "${UNIT_PATH}" ]; then
+        failed "error: exists ${UNIT_PATH}"
     fi
 
     if [ -f "${TEMP_PATH}" ]; then
       rm "${TEMP_PATH}" || failed "failed to delete ${TEMP_PATH}"
     fi
 
-    log_path="${HOME}/Library/Logs/${SVC_NAME}"
-    echo "Creating ${log_path}"
-    mkdir -p "${log_path}" || failed "failed to create ${log_path}"
+    # can optionally use username supplied
+    run_as_user=${arg_2:-$SUDO_USER}
+    echo "Run as user: ${run_as_user}"
 
-    echo Creating ${PLIST_PATH}
-    sed "s/{{User}}/${USER:-$SUDO_USER}/g; s/{{SvcName}}/$SVC_NAME/g; s@{{RunnerRoot}}@${RUNNER_ROOT}@g; s@{{UserHome}}@$HOME@g;" "${TEMPLATE_PATH}" > "${TEMP_PATH}" || failed "failed to create replacement temp file"
-    mv "${TEMP_PATH}" "${PLIST_PATH}" || failed "failed to copy plist"
+    run_as_uid=$(id -u ${run_as_user}) || failed "User does not exist"
+    echo "Run as uid: ${run_as_uid}"
 
-    # Since we started with sudo, runsvc.sh will be owned by root. Change this to current login user.
-    echo Creating runsvc.sh    
+    run_as_gid=$(id -g ${run_as_user}) || failed "Group not available"
+    echo "gid: ${run_as_gid}"
+
+    sed "s/{{User}}/${run_as_user}/g; s/{{Description}}/$(echo ${SVC_DESCRIPTION} | sed -e 's/[\/&]/\\&/g')/g; s/{{RunnerRoot}}/$(echo ${RUNNER_ROOT} | sed -e 's/[\/&]/\\&/g')/g;" "${TEMPLATE_PATH}" > "${TEMP_PATH}" || failed "failed to create replacement temp file"
+    mv "${TEMP_PATH}" "${UNIT_PATH}" || failed "failed to copy unit file"
+
+    # Recent Fedora based Linux (CentOS/Redhat) has SELinux enabled by default
+    # We need to restore security context on the unit file we added otherwise SystemD have no access to it.
+    command -v getenforce > /dev/null
+    if [ $? -eq 0 ]
+    then
+        selinuxEnabled=$(getenforce)
+        if [[ $selinuxEnabled == "Enforcing" ]]
+        then
+            # SELinux is enabled, we will need to Restore SELinux Context for the service file
+            restorecon -r -v "${UNIT_PATH}" || failed "failed to restore SELinux context on ${UNIT_PATH}"
+        fi
+    fi
+    
+    # unit file should not be executable and world writable
+    chmod 664 "${UNIT_PATH}" || failed "failed to set permissions on ${UNIT_PATH}"
+    systemctl daemon-reload || failed "failed to reload daemons"
+    
+    # Since we started with sudo, runsvc.sh will be owned by root. Change this to current login user.    
     cp ./bin/runsvc.sh ./runsvc.sh || failed "failed to copy runsvc.sh"
-    chmod u+x ./runsvc.sh || failed "failed to set permission for runsvc.sh"
+    chown ${run_as_uid}:${run_as_gid} ./runsvc.sh || failed "failed to set owner for runsvc.sh"
+    chmod 755 ./runsvc.sh || failed "failed to set permission for runsvc.sh"
 
-    echo Creating ${CONFIG_PATH}
-    echo "${PLIST_PATH}" > ${CONFIG_PATH} || failed "failed to create .Service file"
+    systemctl enable ${SVC_NAME} || failed "failed to enable ${SVC_NAME}"
 
-    echo "svc install complete"
+    echo "${SVC_NAME}" > ${CONFIG_PATH} || failed "failed to create .service file"
+    chown ${run_as_uid}:${run_as_gid} ${CONFIG_PATH} || failed "failed to set permission for ${CONFIG_PATH}"
 }
 
 function start()
 {
-    echo "starting ${SVC_NAME}"
-    launchctl load -w "${PLIST_PATH}" || failed "failed to load ${PLIST_PATH}"
-    status
+    systemctl start ${SVC_NAME} || failed "failed to start ${SVC_NAME}"
+    status    
 }
 
 function stop()
 {
-    echo "stopping ${SVC_NAME}"
-    launchctl unload "${PLIST_PATH}" || failed "failed to unload ${PLIST_PATH}"
+    systemctl stop ${SVC_NAME} || failed "failed to stop ${SVC_NAME}"    
     status
 }
 
 function uninstall()
 {
-    echo "uninstalling ${SVC_NAME}"
-    stop
-    rm "${PLIST_PATH}" || failed "failed to delete ${PLIST_PATH}"
+    if service_exists; then
+        stop
+        systemctl disable ${SVC_NAME} || failed "failed to disable ${SVC_NAME}"
+        rm "${UNIT_PATH}" || failed "failed to delete ${UNIT_PATH}"
+    else
+        echo "Service ${SVC_NAME} is not installed"
+    fi    
     if [ -f "${CONFIG_PATH}" ]; then
       rm "${CONFIG_PATH}" || failed "failed to delete ${CONFIG_PATH}"
+    fi
+    systemctl daemon-reload || failed "failed to reload daemons"
+}
+
+function service_exists() {
+    if [ -f "${UNIT_PATH}" ]; then
+        return 0
+    else
+        return 1
     fi
 }
 
 function status()
 {
-    echo "status ${SVC_NAME}:"
-    if [ -f "${PLIST_PATH}" ]; then
+    if service_exists; then
         echo
-        echo "${PLIST_PATH}"
+        echo "${UNIT_PATH}"
     else
         echo
         echo "not installed"
         echo
-        return
+        exit 1
     fi
 
-    echo
-    status_out=`launchctl list | grep "${SVC_NAME}"`
-    if [ ! -z "$status_out" ]; then
-        echo Started:
-        echo $status_out
-        echo
-    else
-        echo Stopped
-        echo
-    fi
+    systemctl --no-pager status ${SVC_NAME}
 }
 
 function usage()
@@ -131,6 +157,12 @@ function usage()
     echo
     echo Usage:
     echo "./svc.sh [install, start, stop, status, uninstall]"
+    echo "Commands:"
+    echo "   install [user]: Install runner service as Root or specified user."
+    echo "   start: Manually start the runner service."
+    echo "   stop: Manually stop the runner service."
+    echo "   status: Display status of runner service."
+    echo "   uninstall: Uninstall runner service."
     echo
 }
 
@@ -140,6 +172,7 @@ case $SVC_CMD in
    "uninstall") uninstall;;
    "start") start;;
    "stop") stop;;
+   "status") status;;
    *) usage;;
 esac
 
